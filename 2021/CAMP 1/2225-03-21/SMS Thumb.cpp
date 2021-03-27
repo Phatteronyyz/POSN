89a1df4393d93f870ac1880f98880bbf88be5262
Phattarawat <pattarawat123.k@gmail.com>
2021-03-27 13:22:53 +0700

    Create SMS Thumb.cpp

---
+++ 2021/CAMP 1/2225-03-21/SMS Thumb.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[100];
+int main(){
+    int n,s,m;
+    scanf("%d",&n);
+    scanf("%d %d",&s,&m);
+    printf("%c",((s-2)*3)+(m%3)+2+64);
+
+
+
+
+
+
+
+    return 0;
+}
