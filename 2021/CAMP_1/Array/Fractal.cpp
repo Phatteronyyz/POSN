b581014d46a553a512c2697c45bbfd668156b3fe
Phattarawat <pattarawat123.k@gmail.com>
2021-04-13 09:39:08 +0700

    commit -all

--- 2021/CAMP_1/Array/Fractal.cpp
+++ 2021/CAMP_1/Array/Fractal.cpp
@@ -2,18 +2,31 @@
 using namespace std;
 char a[5000][5000];
 int main() {
-    int n,i,j,k,r,c,rr,cc,t;
+    int n,i,j,k,l,r,c,rr,cc,t;
     scanf("%d",&n);
     a[0][0]=1;
-    for(i=1;i<n;i++){
+    for(i=1;i<=n;i++){
         r=(1<<i)-1;
-        for(j=0;j<(1<<i);j++){
-            a[r][j]=1;
+        c=(1<<i-1);
+        rr=(1<<i-1)-1;
+        cc=(1<<i-2);
+        for(j=0;j<c;j++){
+            a[rr][j]=1;
         }
-
+        for(int i=0;i<rr;i++){
+            for(int j=0;j<cc;j++){
+                a[i+rr+1][cc+j]=a[i][j];
+            }
+        }
     }
 
-    for(i=1;i)
+    for(i=0;i<(1<<n)-1;i++){
+        for(j=0;j<(1<<n-1);j++){
+            if(a[i][j]==1) printf("*");
+            else printf("-");
+        }
+        printf("\n");
+    }
     
     
     return 0;
